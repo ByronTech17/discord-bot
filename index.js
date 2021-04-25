718a25af9ab10606c4b5faf137d0a22d93980a83
EricFoulkes <83133057+EricFoulkes@users.noreply.github.com>
2021-04-25 12:04:54 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -15,7 +15,10 @@ client.on('message', message => {
 		message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
 	} else if (message.content === `${prefix}userinfo`) {
 		message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
+	} else if (message.content === `${prefix}test`) {
+		message.channel.send('Ben is dumb');
 	}
+
 });
 
 client.login(token);
